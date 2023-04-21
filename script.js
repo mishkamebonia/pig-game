039b0516a4ad7cb38f7a3093f35ed9992b690f15
mishkamebonia <60870999+mishkamebonia@users.noreply.github.com>
2023-04-21 18:12:36 +0400

    add keyboard

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const switchPlayer = () => {
   playerSecondSection.classList.toggle('player--active')
 }
 
-rollDice.addEventListener('click', () => {
+const rollBtn = () => {
   if(playing) {
     const randomNumber = Math.trunc(Math.random() * 6 ) + 1
     diceBox.classList.remove('hidden')
@@ -39,10 +39,11 @@ rollDice.addEventListener('click', () => {
       switchPlayer()
     }
   }
-})
+}
 
+rollDice.addEventListener('click', rollBtn)
 
-hold.addEventListener('click', () => {
+const holdBtn = () => {
   if(playing) {
     scores[activePlayer] += currentScore
     document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer]
@@ -56,15 +57,19 @@ hold.addEventListener('click', () => {
       switchPlayer()
     }
   }
-})
+}
 
-NewGame.addEventListener('click', () => {
+hold.addEventListener('click', holdBtn)
+
+const restartGame = () => {
   playing = true
 
   activePlayer = 0
   currentScore = 0
   scores[0] = currentScore
   scores[1] = currentScore
+
+  diceBox.classList.add('hidden')
 
   document.querySelector(`#score--0`).textContent = 0
   document.querySelector(`#score--1`).textContent = 0
@@ -75,4 +80,16 @@ NewGame.addEventListener('click', () => {
   document.querySelector(`.player--1`).classList.remove('player--winner')
   document.querySelector(`.player--0`).classList.add('player--active')
   document.querySelector(`.player--1`).classList.remove('player--active')
-}) 
+}
+
+NewGame.addEventListener('click', restartGame)
+
+document.addEventListener('keydown', (e) => {
+  if(e.key === 'Enter') {
+    rollBtn()
+  }else if(e.key === ' ') {
+    holdBtn()
+  }else if(e.key === 'r' || e.key === 'R') {
+    restartGame()
+  }
+})
